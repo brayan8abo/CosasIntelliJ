cda228e85fc6d25728608edcc906c107fd0ee633
Brayan8abo <brayan8abo@outlook.es>
2024-04-03 01:22:55 +0200

    update

--- src/main/java/Ejercicio1/reto1_2.java
+++ src/main/java/Ejercicio1/reto1_2.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class reto1_2 {
 //	Ejercicio 2
 //	Escribir un programa Java que le pida al usuario el nombre de un fichero y el nombre de un directorio. CHECK
+//	Compruebe si el fichero existe. Si no existe devuelve un mensaje de error y sale. CHECK
 
-//	Compruebe si el fichero existe. Si no existe devuelve un mensaje de error y sale.
 //	Comprueba si se puede leer y si se puede escribir en él
 //	Visualiza la longitud del fichero
 //	Borra el fichero
@@ -20,15 +20,26 @@ public class reto1_2 {
 		System.out.println("Ingresa el nombre de un directorio");
 		String directorio = leer.nextLine();
 
-		File files = new File(fichero, directorio);
+		File files = new File(directorio, fichero);
 
 		if (files.exists()) {
-			System.out.println("Existe");
-			if (files.isFile()){
+			if (files.isFile()) {
 				System.out.println("El fichero SÍ existe");
 			} else {
 				System.err.println("NO EXISTE");
 			}
+			if (files.canRead()) {
+				System.out.println("SÍ se puede leer");
+			} else {
+				System.err.println("NO puede ser leido");
+			}
+			if (files.canWrite()){
+				System.out.println("Se puede escribir aquí");
+			} else {
+				System.err.println("NO se puede escribir");
+			}
 		}
+
 	}
 }
+
