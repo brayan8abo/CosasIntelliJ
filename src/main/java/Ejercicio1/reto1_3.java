43f19ea0f99f48ca81042495afd02bd01edbdf1a
Brayan8abo <brayan8abo@outlook.es>
2024-04-03 20:06:38 +0200

    update ejercicio1.

---
+++ src/main/java/Ejercicio1/reto1_3.java
@@ -0,0 +1,53 @@
+package Ejercicio1;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class reto1_3 {
+//    Escribir un programa Java que le pida al usuario el nombre de un directorio y de un fichero. CHECK
+//    Compruebe si el fichero existe dentro del directorio. Si no existe devuelve un mensaje de error y sale. CHECK
+
+//    Listar los subdirectorios del directorio
+//    Listar los ficheros del directorio
+//    Calcular el tamaño total de los ficheros de un directorio
+//    Listar los ficheros de un directorio que empiecen por una letra en particular (utilizar el método charAt())
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingresa el nombre de un directorio");
+        String directorio = leer.nextLine();
+        System.out.println("Ingresa el nombre de un fichero");
+        String fichero = leer.nextLine();
+
+        File archivos = new File(directorio, fichero);
+
+
+        File[] subDirectorios = archivos.listFiles();
+
+        if (archivos.exists()) {
+            if (archivos.isDirectory()) {
+                System.out.println("Es un directorio");
+            } else {
+                System.err.println("No es un directorio");
+            }
+            if (archivos.isFile()) {
+                System.out.println("Si existen dentro del directorio");
+            } else {
+                System.err.println("No existe dentro del directorio");
+            }
+            if (archivos.isDirectory()) {
+                for (File subDir : subDirectorios) {
+                    if (subDir.isDirectory()) {
+                        System.out.println("Los subdirectorios de este directorio son: " + subDir.getName());
+                    }
+
+
+                }
+            }
+        }
+
+
+    }
+
+}
