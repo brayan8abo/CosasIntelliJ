0460fe45f13622358b83319547e35d57fe9590a5
Brayan8abo <brayan8abo@outlook.es>
2024-04-05 18:14:36 +0200

    update ejemplos

--- src/main/java/Ejercicio1/reto1_3.java
+++ src/main/java/Ejercicio1/reto1_3.java
@@ -12,78 +12,76 @@ public class reto1_3 {
 //    Calcular el tamaño total de los ficheros de un directorio
 //    Listar los ficheros de un directorio que empiecen por una letra en particular (utilizar el método charAt())
 
-	public static void main(String[] args) {
-		Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
 
-		System.out.println("Ingresa el nombre de un directorio");
-		String directorio = leer.nextLine();
-		System.out.println("Ingresa el nombre de un fichero");
-		String fichero = leer.nextLine();
+        System.out.println("Ingresa el nombre de un directorio");
+        String directorio = leer.nextLine();
+        System.out.println("Ingresa el nombre de un fichero");
+        String fichero = leer.nextLine();
 
-		File archivos = new File(directorio, fichero);
+        File archivos = new File(directorio, fichero);
 
 
-		File[] subDirectorios = archivos.listFiles();
-		File[] ficheros = archivos.listFiles();
+        File[] subDirectorios = archivos.listFiles();
+        File[] ficheros = archivos.listFiles();
 
-		if (archivos.exists()) {
-			if (archivos.isDirectory()) {
-				System.out.println("Es un directorio");
-			} else {
-				System.err.println("No es un directorio");
-			}
-			if (archivos.isFile()) {
-				System.out.println("Si existen dentro del directorio");
-			} else {
-				System.err.println("No existe dentro del directorio");
-			}
-			if (archivos.isDirectory()) {
-				for (File subDir : subDirectorios) {
-					if (subDir.isDirectory()) {
-						System.out.println("Los subdirectorios de este directorio son: " + subDir.getName());
-					}
-//                    listarArchivos(subDir);
-				}
-			}
-			if (archivos.isFile()) {
-				for (File file : ficheros) {
-					if (file.isFile()) {
-						System.out.println("Los ficheros del directorio son: " + file.getName());
-					}
-				}
+        if (archivos.exists()) {
+            if (archivos.isDirectory()) {
+                System.out.println("Es un directorio");
+            } else {
+                System.err.println("No es un directorio");
+            }
+            if (archivos.isFile()) {
+                System.out.println("Si existen dentro del directorio");
+            } else {
+                System.err.println("No existe dentro del directorio");
+            }
+            if (archivos.isDirectory()) {
+                for (File subDir : subDirectorios) {
+                    if (subDir.isDirectory()) {
+                        System.out.println("Los subdirectorios de este directorio son: " + subDir.getName());
+                    }
+                    listarArchivos(subDir);
+                }
+            }
+            if (archivos.isFile()) {
+                assert ficheros != null;
+                for (File file : ficheros) {
+                    if (file.isFile()) {
+                        System.out.println("Los ficheros del directorio son: " + file.getName());
+                    }
+                }
 
-			}
-		}
-//            File [] longitudes = archivos.listFiles();
-//            int suma =0;
-//
-//
-//                for (File longitud: longitudes){
-//                    if (longitud.isFile()){
-//                        suma += longitud.length();
-//                    }
-//
-//                }
-//                System.out.println("La suma de todos los ficheros es: " + suma);
-//
-//
+            }
+        }
+        File[] longitudes = archivos.listFiles();
+        int suma = 0;
 
-	}
+
+        for (File longitud : longitudes) {
+            if (longitud.isFile()) {
+                suma += longitud.length();
+            }
+        }
+        System.out.println("La suma de todos los ficheros es: " + suma);
+
+
+    }
+
+    public static void listarArchivos(File directorio) {
+        if (directorio.isFile()) {
+            System.out.println("Archivos en el directorio " + directorio.getAbsolutePath() + " ");
+            File[] archivos = directorio.listFiles();
+            if (archivos != null) {
+                for (File archivo : archivos) {
+                    if (archivo.isDirectory()) {
+                        listarArchivos(archivo); // Llamada recursiva si es un directorio
+                    } else {
+                        System.out.println(archivo.getName());
+                    }
+                }
+            }
+        }
+    }
 }
-
-//    public static void listarArchivos(File directorio) {
-//        if (directorio.isFile()) {
-//            System.out.println("Archivos en el directorio " + directorio.getAbsolutePath() + " ");
-//            File[] archivos = directorio.listFiles();
-//            if (archivos != null) {
-//                for (File archivo : archivos) {
-//                    if (archivo.isDirectory()) {
-//                        listarArchivos(archivo); // Llamada recursiva si es un directorio
-//                    } else {
-//                        System.out.println(archivo.getName());
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
---
+++ src/main/java/Ejercicios2/Reto1_2.java
@@ -0,0 +1,54 @@
+package Ejercicios2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+//Hacer un programa que lea el fichero de texto, “texto.txt”, y genere otro llamado “invertido.txt”, en el que se guardarán las frases del primero pero invertidas.
+//        Mostrar en pantalla el nuevo fichero.
+//        Ejemplo:
+//        texto.txt Esto es un ejemplo
+//        invertido.txt olpmeje nu se otsE
+public class Reto1_2 {
+    public static void main(String[] args) {
+        leer();
+        escribir();
+
+    }
+
+    public static void leer() {
+        try (FileReader entrada = new FileReader("C:\\pruebas\\archivos\\texto.txt")) {
+            BufferedReader salida = new BufferedReader(entrada);
+            try {
+                String lineas;
+                while ((lineas = salida.readLine()) != null)
+                    System.out.println(lineas);
+                ArrayList<String> texto = new ArrayList<>();
+                texto.add(lineas);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void escribir(ArrayList) {
+
+
+        FileWriter escribiendo = null;
+        try {
+            escribiendo = new FileWriter("C:\\dir2\\textoInvertido.txt");
+            for (int i = texto.size() - 1; i >= 0; i--) {
+                escribiendo.write(texto.size());
+            }
+            escribiendo.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
