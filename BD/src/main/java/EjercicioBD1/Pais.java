ef48253c683ead4c61cd93feec70f64b762644af
Brayan8abo <brayan8abo@outlook.es>
2024-04-25 14:51:04 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Entornos_de_desarrollo/src/main/java/ActividadEduardoUML/Socios.java

---
+++ BD/src/main/java/EjercicioBD1/Pais.java
@@ -0,0 +1,10 @@
+package EjercicioBD1;
+
+public class Pais {
+    public static void main(String[] args) {
+        try {
+            final String instruccionCreacionSQL = "create database"
+        }
+
+    }
+}
