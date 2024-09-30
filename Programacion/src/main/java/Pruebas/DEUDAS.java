830a4a4684768c960c0aa6e851a7c21f69214280
Brayan8abo <brayan8abo@outlook.es>
2024-09-30 12:30:26 +0200

    update casa 888

---
+++ Programacion/src/main/java/Pruebas/DEUDAS.java
@@ -0,0 +1,21 @@
+package Pruebas;
+
+public class DEUDAS {
+	public static void main(String[] args) {
+
+		int [] deudas = {
+				60,
+				66,
+				80,
+				24,
+				50
+		};
+
+		int suma = deudas[0] + deudas[1] + deudas[2] + deudas[3] + deudas[4];
+
+
+		System.out.println("Las deudas que tienes al momento son: " + suma);
+
+
+	}
+}
